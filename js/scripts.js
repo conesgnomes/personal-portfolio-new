f077b9f48a2267d5c8e49163405d09628bb16e82
Connor Larsen <Connor@Cons-Mac.local>
2017-06-16 13:43:15 -0700

    style animation effects on nav bar

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,17 @@ $(function() {
     $("html,body").animate({
       scrollTop: $("nav").offset().top}, "slow");
     });
+
+  //   // Check the initial Poistion of the Sticky Header
+  // var stickyHeaderTop = $('.nav-bar').offset().top;
+  //
+  // $(window).scroll(function(){
+  //   if( $(window).scrollTop() > stickyHeaderTop ) {
+  //     $('.nav-bar').css({position: 'fixed', top: '0px', justify-content: 'space-between'});
+  //     $('.stickyalias').css('display', 'block');
+  //   } else {
+  //     $('.nav-bar').css({position: 'static', top: '0px'});
+  //     $('.stickyalias').css('display', 'none');
+  //   }
+  // });
 });
