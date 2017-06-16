389e5531369e305066a82b418da1848b8ebea70b
Connor Larsen <Connor@Cons-Mac.local>
2017-06-16 13:54:37 -0700

    finish styling nav bar adding fixed to top and transparent background on scroll down

--- js/scripts.js
+++ js/scripts.js
@@ -4,16 +4,13 @@ $(function() {
       scrollTop: $("nav").offset().top}, "slow");
     });
 
-  //   // Check the initial Poistion of the Sticky Header
-  // var stickyHeaderTop = $('.nav-bar').offset().top;
-  //
-  // $(window).scroll(function(){
-  //   if( $(window).scrollTop() > stickyHeaderTop ) {
-  //     $('.nav-bar').css({position: 'fixed', top: '0px', justify-content: 'space-between'});
-  //     $('.stickyalias').css('display', 'block');
-  //   } else {
-  //     $('.nav-bar').css({position: 'static', top: '0px'});
-  //     $('.stickyalias').css('display', 'none');
-  //   }
-  // });
+  var stickyHeaderTop = $('.nav-bar').offset().top;
+
+  $(window).scroll(function(){
+    if( $(window).scrollTop() > stickyHeaderTop ) {
+      $('.nav-bar').css({background: 'rgba(255, 255, 255, 0.6)'});
+    } else {
+      $('.nav-bar').css({background: 'none'});
+    }
+  });
 });
