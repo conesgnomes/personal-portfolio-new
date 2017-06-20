919d0781337f7972a93942c534c3a8dc4e2f4ea3
Connor Larsen <Connor@Cons-Mac.local>
2017-06-19 22:21:03 -0700

    use refills to create paralax effect on header background img

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,40 @@ $(function() {
 
   // background image on header
 
-  $(window).scroll(function() {
-    var scrolledY = $(window).scrollTop();
-    $('header').css('background-position', 'left ' + ((scrolledY)) + 'px');
+  // $(window).scroll(function() {
+  //   var scrolledY = $(window).scrollTop();
+  //   $('header').css('background-position', 'left ' + ((scrolledY)) + 'px');
+  // });
+
+  if ($("#js-parallax-window").length) {
+    parallax();
+  }
+// });
+
+  $(window).scroll(function(e) {
+    if ($("#js-parallax-window").length) {
+      parallax();
+    }
   });
+
+  function parallax(){
+    if( $("#js-parallax-window").length > 0 ) {
+      var plxBackground = $("#js-parallax-background");
+      var plxWindow = $("#js-parallax-window");
+
+      var plxWindowTopToPageTop = $(plxWindow).offset().top;
+      var windowTopToPageTop = $(window).scrollTop();
+      var plxWindowTopToWindowTop = plxWindowTopToPageTop - windowTopToPageTop;
+
+      var plxBackgroundTopToPageTop = $(plxBackground).offset().top;
+      var windowInnerHeight = window.innerHeight;
+      var plxBackgroundTopToWindowTop = plxBackgroundTopToPageTop - windowTopToPageTop;
+      var plxBackgroundTopToWindowBottom = windowInnerHeight - plxBackgroundTopToWindowTop;
+      var plxSpeed = 0.35;
+
+      plxBackground.css('top', - (plxWindowTopToWindowTop * plxSpeed) + 'px');
+    }
+  };
 
   // auto scroll to bio on 'enter' button click
 
